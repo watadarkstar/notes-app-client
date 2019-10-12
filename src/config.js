818fada28f4c4fbee16da1f9f2e69278e18934c6
Adrian Carolli <adrian.carolli1@gmail.com>
2019-10-12 16:19:29 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-adrian-carolli"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://4oy3s1r6ld.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_wtLDu66rW",
+    APP_CLIENT_ID: "1amtpnp3p0s6e02t4okioarfio",
+    IDENTITY_POOL_ID: "us-east-2:1e59dacd-2fa0-42d0-960f-a66aaaf48871"
+  }
+};
